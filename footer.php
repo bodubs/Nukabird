97e6a97941117bfef0248f7c80a724db53173e6e
Robert WarrenIII <robert.warren-iii@uvm.edu>
2020-12-17 10:57:27 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,6 @@
+<!-- ################ Closing Tags for main-container and main divs ################### -->
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,25 @@
+<div id="main-container">
+    <div id="main">
+
+        <header>
+            <div id="nuka-logo-div">
+                <div id="nb-polygon-1-div">
+                    <img src="images/logos/nukabird-logo/polygon.png" alt="Nukabird Logo Polygon 1" id="nb-polygon-1"/>
+                </div>
+                <div id="nb-lettering-div">
+                    <img src="images/logos/nukabird-logo/nukabird-lettering.png" alt="Nukabird Logo Lettering" id="nb-lettering">
+                </div>
+                <div id="nb-polygon-2-div">
+                    <img src="images/logos/nukabird-logo/polygon.png" alt="Nukabird Logo Polygon 1" id="nb-polygon-2"/>
+                </div>
+            </div>
+            <nav>
+                <ul class="right-nav">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="clients.php">Clients</a></li>
+                    <li><a href="projects.php">Projects</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
